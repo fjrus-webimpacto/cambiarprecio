20c6c37023c7fe60aa3926eef05ac7b91bc9eb39
FJ Rus <fjrus@webimpacto.es>
2019-05-21 17:22:26 +0200

    14

--- cambiarprecio.php
+++ cambiarprecio.php
@@ -151,7 +151,7 @@ class CambiarPrecio extends Module
         }
     }
     
-    public function registrosExcluidos($id_registro, $id_producto)
+    public function preciosExcluidos($id_registro, $id_producto)
     {
         $sql = 'SELECT `id_cambioprecio`, `id_excluido`
         FROM `'._DB_PREFIX_.'excluido`
@@ -377,15 +377,15 @@ class CambiarPrecio extends Module
                 $nuevoDato2 = new Cambiar($dato['id_cambioprecio']);
                 foreach ($productos as $producto) {
                     $precioproducto = new Product($producto['id_product']);
-                    if (!$this->registrosExcluidos($nuevoDato2->id_cambioprecio, $precioproducto->id)) {
+                    if (!$this->preciosExcluidos($nuevoDato2->id_cambioprecio, $precioproducto->id)) {
                         if ($nuevoDato2->tipo == "porcentaje" && $nuevoDato2->opcion == "disminuir") {
-                            $precioproducto->price =  $precioproducto->price + ($precioproducto->price * $nuevoDato2->cantidad/100);
+                            $precioproducto->price =+ ($precioproducto->price * $nuevoDato2->cantidad/100);
                         } elseif ($nuevoDato2->tipo == "cantidad" && $nuevoDato2->opcion == "disminuir") {
-                            $precioproducto->price =  $precioproducto->price + $nuevoDato2->cantidad;
+                            $precioproducto->price =+ $nuevoDato2->cantidad;
                         } elseif ($nuevoDato2->tipo == "porcentaje" && $nuevoDato2->opcion == "aumentar") {
-                            $precioproducto->price =  $precioproducto->price - ($precioproducto->price * $nuevoDato2->cantidad/100);
+                            $precioproducto->price =- ($precioproducto->price * $nuevoDato2->cantidad/100);
                         } elseif ($nuevoDato2->tipo == "cantidad" && $nuevoDato2->opcion == "aumentar") {
-                            $precioproducto->price = $precioproducto->price - $nuevoDato2->cantidad;
+                            $precioproducto->price =- $nuevoDato2->cantidad;
                         }
                         $precioproducto->save();
                     }
@@ -393,7 +393,7 @@ class CambiarPrecio extends Module
             }
         }
     }
-    
+
     /**
      * Save form data.
      */
